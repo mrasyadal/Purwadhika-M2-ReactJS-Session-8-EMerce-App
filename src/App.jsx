88306260d905b719af091dd59360e9fa1d6397ef
mrasyadal <m.rasyad.a.l@gmail.com>
2021-08-23 12:17:15 +0700

    Commit all

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
+import Home from "./pages/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import Admin from "./pages/Admin";
+import Cart from "./pages/Cart";
+import History from "./pages/History";
+import ProductDetail from "./pages/ProductDetail";
+import MyNavbar from "./components/MyNavbar";
+
+class App extends React.Component {
+	render() {
+		return (
+			<BrowserRouter>
+				<MyNavbar />
+
+				<Switch>
+					{/* sesuatu yg di-import bernama `component` */}
+					<Route component={Login} path="/login" />
+					<Route component={Register} path="/register" />
+					<Route component={Admin} path="/admin" />
+					<Route component={Cart} path="/cart" />
+					<Route component={History} path="/history" />
+					<Route component={ProductDetail} path="/product-detail" />
+					<Route component={Home} path="/" />
+				</Switch>
+			</BrowserRouter>
+		);
+	}
+}
+
+export default App;
